f76541e70e8262f178355085efc89a9fa9c5e1e5
LDClaudia07 <Lpzclaudy@outlook.com>
2024-01-10 21:02:24 -0600

    Modificación(Se le agrero imagenes)

--- src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -25,11 +25,15 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
         if(getMessageText().equals("/start")){
             setUserGlory(0);
+
+            sendPhotoMessageAsync("step_1_pic");
             sendTextMessageAsync(STEP_1_TEXT,
                     Map.of("Hackear la nevera",  "step_1_btn"));
         }
         if (getCallbackQueryButtonKey().equals("step_1_btn")){
             setUserGlory(20);
+
+            sendPhotoMessageAsync("step_2_pic");
             sendTextMessageAsync(STEP_2_TEXT,
                     Map.of("¡Toma una salchicha +20 de fama", "step_2_btn",
                             "¡Tomar un pescado! +20 de fama", "step_2_btn",
@@ -38,12 +42,16 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         }
         if (getCallbackQueryButtonKey().equals("step_2_btn")){
             setUserGlory(20);
+
+            sendPhotoMessageAsync("step_3_pic");
             sendTextMessageAsync(STEP_3_TEXT,
                     Map.of("Hackear al robot aspiradora", "step_3_btn"));
 
         }
         if (getCallbackQueryButtonKey().equals("step_3_btn")){
             setUserGlory(30);
+
+            sendPhotoMessageAsync("step_4_pic");
             sendTextMessageAsync(STEP_4_TEXT,
                     Map.of("¡Enviar al robot aspiradora a por comida! +30 de fama", "step_4_btn",
                             "¡Dar un paseo en el robot aspiradora! +30 de fama", "step_4_btn",
@@ -52,20 +60,35 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         }
         if (getCallbackQueryButtonKey().equals("step_4_btn")){
             addUserGlory(30);
-            sendTextMessageAsync(STEP_5_TEXT, Map.of("¡Ponerse la GoPro!  +40 de fama", "step_5_btn"));
+
+            sendPhotoMessageAsync("step_5_pic");
+            sendTextMessageAsync(STEP_5_TEXT, Map.of("¡Encender y ponerse la GoPro!", "step_5_btn"));
         }
         if (getCallbackQueryButtonKey().equals("step_5_btn")){
             addUserGlory(40);
-            sendTextMessageAsync(STEP_6_TEXT, Map.of("¡Elegir recompensa!", "step_6_btn"));
+
+            sendPhotoMessageAsync("step_6_pic");
+            sendTextMessageAsync(STEP_6_TEXT,
+                    Map.of("¡Correr por los tejados, grabar con la GoPro! +40 de fama", "step_6_btn",
+                    "¡Atacar a otros gatos desde tu escondite con la GoPro! + 40 de fama", "step_6_btn",
+                    "¡Atacar a los perros desde tu escondite con la GoPro! +40 de fama", "step_6_btn"));
         }
         if (getCallbackQueryButtonKey().equals("step_6_btn")){
-            addUserGlory(50);
-            sendTextMessageAsync(STEP_7_TEXT, Map.of("¡Hackea la contraseña de la computadora! +50 de fama", "step_7_btn"));
+            addUserGlory(40);
+
+            sendPhotoMessageAsync("step_7_pic");
+            sendTextMessageAsync(STEP_7_TEXT, Map.of("¡Hackea la contraseña de la computadora!", "step_7_btn"));
         }
         if (getCallbackQueryButtonKey().equals("step_7_btn")){
-            sendTextMessageAsync(STEP_8_TEXT, Map.of("Finalizar juego!", "step_8_btn"));
+            addUserGlory(50);
+
+            sendPhotoMessageAsync("step_8_pic");
+            sendTextMessageAsync(STEP_8_TEXT, Map.of("¡Salir al patio!", "step_8_btn"));
         }
         if (getCallbackQueryButtonKey().equals("step_8_btn")){
+            addUserGlory(50);
+
+            sendPhotoMessageAsync("final_pic");
             sendTextMessageAsync(FINAL_TEXT);
         }
 
